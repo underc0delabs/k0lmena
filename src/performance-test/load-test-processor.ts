c358cd48065aae77ed180d5c18008b6898b786f7
DInno <dinno.seven@gmail.com>
2025-09-29 16:43:26 -0300

    Update performance tests

--- src/performance-test/load-test-processor.ts
+++
@@ -1,9 +0,0 @@
-module.exports = { googleSearch };
-
-async function googleSearch(page) {
-    await page.goto('https://www.google.com');
-    await page.waitForSelector('//textarea[contains(@name,"q")]');
-    await page.fill('//textarea[contains(@name,"q")]', 'Underc0de');
-    await page.keyboard.press('Enter');
-    await page.waitForTimeout(3000);
-}
